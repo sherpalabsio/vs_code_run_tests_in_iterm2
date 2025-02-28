607da7c7a2e8516cdfb1e51ac8cd5a7741f43a95
Peter Toth <tothpeter08@gmail.com>
2025-03-01 00:49:28 +0700

    Replace the full path with relative one for the final command

--- src/runners.js
+++ src/runners.js
@@ -1,6 +1,7 @@
 const vscode = require('vscode');
 const { getTestCommand } = require('./testCommandHelper');
 const iTerm2 = require('./iterm2');
+const path = require('path');
 
 function runAll() {
   const editor = vscode.window.activeTextEditor;
@@ -17,7 +18,7 @@ function runCurrentFile() {
   const editor = vscode.window.activeTextEditor;
   if (!editor) return;
 
-  const filePath = editor.document.uri.fsPath;
+  const filePath = getCurrentFilePath();
   const command = getTestCommand(filePath, editor.document.languageId);
   const finalCommand = `${command} ${filePath}`;
 
@@ -28,13 +29,24 @@ function runAtCursor() {
   const editor = vscode.window.activeTextEditor;
   if (!editor) return;
 
-  const filePath = editor.document.uri.fsPath;
+  const filePath = getCurrentFilePath();
   const lineNumber = editor.selection.active.line + 1; // +1 because VSCode uses 0-based line numbers
   const fileAndLine = `${filePath}:${lineNumber}`;
   const command = getTestCommand(filePath, editor.document.languageId);
   const finalCommand = `${command} ${fileAndLine}`;
 
   iTerm2.run(finalCommand);
+}
+
+function getCurrentFilePath() {
+  const editor = vscode.window.activeTextEditor;
+
+  const filePath = editor.document.uri.fsPath;
+  if (vscode.workspace.rootPath) {
+    return path.relative(vscode.workspace.rootPath, filePath);
+  } else {
+    return filePath;
+  }
 }
 
 module.exports = {
